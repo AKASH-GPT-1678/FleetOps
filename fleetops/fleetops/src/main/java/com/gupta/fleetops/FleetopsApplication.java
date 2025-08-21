4dc0f4844a43a3e020fb38f8967f09859ae1ae63
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-22 03:04:10 +0530

    delivery location

--- fleetops/fleetops/src/main/java/com/gupta/fleetops/FleetopsApplication.java
+++ fleetops/fleetops/src/main/java/com/gupta/fleetops/FleetopsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
-@EnableRedisRepositories
 public class FleetopsApplication {
 
 	public static void main(String[] args) {
--- fleetops/fleetops/src/main/java/com/gupta/fleetops/repository/DeliveryLocationRepository.java
+++ fleetops/fleetops/src/main/java/com/gupta/fleetops/repository/DeliveryLocationRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Qualifier("deliveryLocationRepository")
 public interface DeliveryLocationRepository extends CrudRepository<DeliveryLocation, String> {
 
     List<DeliveryLocation> findAll();
